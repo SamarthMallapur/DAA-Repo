2e57f107438c48de88aedbb2bb90714de6750741
Samarth Mallapur <143846897+SamarthMallapur@users.noreply.github.com>
2024-12-29 21:16:17 +0530

    Create Bubble sort.cpp

---
+++ Codes/Algorithms/Bubble sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+void bubble_sort(int n, int *a)
+{
+    int i,j,temp=0;
+    for(i=0;i<=n-2;i++) {
+        for (j=0;j<=n-2-i;j++) {
+            if(a[j+1]<a[j]) {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a[10] = {2,6,8,9,4};
+    int n=5;
+
+    bubble_sort(n, a);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+
+    return 0;
+}
