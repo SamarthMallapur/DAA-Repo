982a6444a0a301ebe86a47775a3ff5fcccf01754
Samarth Mallapur <143846897+SamarthMallapur@users.noreply.github.com>
2024-12-29 21:07:19 +0530

    Create Array.cpp

---
+++ Codes/Data structures/Array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5]={0,1,2,3,4};//Implementing an array of size 5
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<endl;//Printing the contents of array by addressing the index.
+    }
+}
